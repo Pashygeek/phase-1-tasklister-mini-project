b723de0491a6f91468c00a91f3212c16db020e82
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-04-07 14:34:21 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const tasks = document.getElementById('tasks');
+  const submitBtn = document.getElementsByTagName('input')[1]
+  const form = document.getElementsByTagName('input')[0]
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    //Input variable
+    const addedValue = document.getElementById('new-task-description').value
+
+    //Creating element p and remove button
+    var p = document.createElement('p')
+    p.textContent = `${addedValue}`
+    const removeBtn = document.createElement('button')
+    removeBtn.textContent = 'x'
+    removeBtn.style.cursor = 'pointer'
+
+    //Appending button to p
+    p.appendChild(removeBtn)
+    //Appending p to tasks
+    tasks.appendChild(p)
+    
+    //Making removeBtn be able to remove p
+    removeBtn.addEventListener('click', removeParent)
+    })
+    function removeParent(e) {
+      e.target.parentNode.remove()
+    }
 });
